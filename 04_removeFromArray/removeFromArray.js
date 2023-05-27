6516ae5c3106675c28e69a02458c09b830bbea38
James Goodloe <jag0007@auburn.edu>
2023-05-26 21:06:27 -0500

    removeFromArray additions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    let filteredArray = array;
+    for (let n of values) {
+        filteredArray = filteredArray.filter(x => !(x === n));
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || type) {
+        return 'ERROR';
+    }    
 
+    var small = a < b ? a : b;
+    var large = a < b ? b : a;
+    let sum = 0;
+    for (i = small; i <= large; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
